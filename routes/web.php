d64795356fd285cd085b73da014b08964e025394
Anicet21 <anicetafagnon47@gmail.com>
2020-09-17 15:31:23 +0100

    J'ai créé le formulaire d'ajout des membres avec son controller et les méthodes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+
+/*Auth::routes();*/
+Route::get('/', 'App\Http\Controllers\Admin\ToganController@index');
+Route::post('/', 'App\Http\Controllers\Admin\ToganController@store')->name('store');
+
+/*Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::group(['namespace'=>'App\Http\Controllers\Admin', 'prefix'=>'admin'], function(){
+Route::get('/togan', 'ToganController@index');
 });
+Route::get('login', ['as'=>'login','uses'=>'App\Http\Controllers\Auth\LoginController']);*/
